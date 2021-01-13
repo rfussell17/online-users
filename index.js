6a9390ef014fb54043840bced5eab08f437cf4a5
Robin Fussell <rfussell17@gmail.com>
2021-01-13 11:28:53 -0800

    mobile layout for sidebar functional

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 async function getUsers() {
   let response = await fetch("users.json")
   let users = await response.json()
-  
   return users
+}
+
+function getUserDiv(user) {
+  return `<div class="my-online-user">
+      <div class="user-username">${user.username}</div>
+      <div class="user-online"></div>
+  </div>`
 }
 
 getUsers().then(users => {
   let sampleUser = users[0]
-  let userDiv = `
-  <div class="my-online-user">
-    <div class="user-username">${sampleUser.username}</div>
-    <div class="user-online"></div> 
+  
+  document.body.innerHTML = `
+  <div class="header"></div>
+  <div class="my-online-users">
+      ${users.map(user => getUserDiv(user)).join('')}
+  </div>
+  <div class="main-content">Chat App</div>
+  <div class="footer"></div>
   `
-
-  document.body.innerHTML = userDiv
-
 })
